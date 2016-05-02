26da6c25b08ccf54716fbfb6d69fac4798809d90
Morteza Hosseinizade <morteza.2128@gmail.com>
2016-05-02 21:08:45 +0430

    -Navigate to house detail view from map vc

--- Narengi/VC/Map/CityDetailMapViewController.m
+++ Narengi/VC/Map/CityDetailMapViewController.m
@@ -57,44 +57,40 @@
     [self.navigationController setNavigationBarHidden:NO animated:YES];
 }
 
-//- (BOOL)mapView:(GMSMapView *)mapView didTapMarker:(GMSMarker *)marker{
-//    
-//    return YES;
-//}
-//
-//- (UIView *)mapView:(GMSMapView *)mapView markerInfoContents:(GMSMarker *)marker{
-//    
-//    NSLog(@"the post title is :%@ %@",marker.userData,marker.title);
-//    
-//    UIView *view =  [[UIView alloc] initWithFrame:CGRectMake(0, 0, 120, 70)]; // Your Created Custom View XIB.
-//    
-//    UILabel *theLabel = [[UILabel alloc]initWithFrame:CGRectMake(10, 50,150, 50)];// Create a Custom Label and add it on the custom view.
-//    theLabel.text = marker.title; // marker.title is the title of pin.
-//    [view addSubview:theLabel];
-//    
-//    return view;
-//}
-//
+#pragma mark - map
+
+-(void)mapView:(GMSMapView *)mapView didTapOverlay:(GMSOverlay *)overlay
+{
+    
+    
+}
+
+-(void)mapView:(GMSMapView *)mapView didTapInfoWindowOfMarker:(GMSMarker *)marker{
+
+    AroundPlaceObject *aroundObj = (AroundPlaceObject *)marker.userData;
+    
+    [self goTodetailWithUrl:aroundObj.urlStr andWithType:aroundObj.type];
+    
+}
 -(void)changeGeo{
     
     [self  makeGeos ];
-    NSMutableArray *muaArr = [[NSMutableArray alloc] init];
     
     [self.houseArr enumerateObjectsUsingBlock:^(AroundPlaceObject  *obj, NSUInteger idx, BOOL * _Nonnull stop) {
        
         if (idx<3) {
             
-            HouseObject *house = obj.houseObject;
-            house.geoObj.lat = [self.arr[idx *2] doubleValue];
-            house.geoObj.lng = [self.arr[idx *2 +1] doubleValue];
+            GMSMarker *sydneyMarker = [[GMSMarker alloc] init];
             
-            [muaArr addObject:house];
+            sydneyMarker.position = CLLocationCoordinate2DMake((CLLocationDegrees)obj.houseObject.geoObj.lat, (CLLocationDegrees)obj.houseObject.geoObj.lng);
+            
+            sydneyMarker.title    = obj.houseObject.name;
+            sydneyMarker.userData = obj;
+            sydneyMarker.map      = mapView_;
 
         }
     }];
-    self.houseArr = [muaArr copy];
     
-    [self addPointsToMap];
     
     
 }
@@ -105,24 +101,5 @@
     self.arr = @[@35.756934, @51.411119,@35.753451, @51.410593,@35.763259, @51.400009,@35.763259, @51.400009];
 }
 
--(void)addPointsToMap{
-
-
-   // [mapView_ clear];
-    for (HouseObject *house in self.houseArr) {
-
-        GMSMarker *sydneyMarker = [[GMSMarker alloc] init];
-
-        sydneyMarker.position = CLLocationCoordinate2DMake((CLLocationDegrees)house.geoObj.lat, (CLLocationDegrees)house.geoObj.lng);
-
-        sydneyMarker.userData = house;
-        sydneyMarker.title    = house.name;
-
-        sydneyMarker.map      = mapView_;
-//        sydneyMarker.tappable = YES;
-
-    }
-
-}
 
 @end
